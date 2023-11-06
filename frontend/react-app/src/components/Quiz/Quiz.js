ed8f5b60758023ef18bbd8a0e17289a2f455d3f1
Faldho23 <faldho.wirianandaho@gmail.com>
2023-11-07 01:02:10 +0800

    css adjusting

--- frontend/react-app/src/components/Quiz/Quiz.js
+++ frontend/react-app/src/components/Quiz/Quiz.js
@@ -9,9 +9,16 @@ const Quiz = () => {
 
     return (  
         <div className="quiz-start">
-            <div>Find out your best course!</div>
-            <img src="img/DataScientist.png" alt="" className="quiz-front" />
-            <button onClick={handleClick} className="button-start">Start Quiz</button>
+            <div className='container-1'>
+                <div class="box1">
+                    <div className="quiz-title">Discover Your Data Potential !<br /></div>
+                    <div className='quiz-desc'>Take our quiz to find out now!</div>
+                </div>
+                <div class="box2"><button onClick={handleClick} className="button-start">Start Quiz</button></div>
+            </div>
+            <div className='container-2'>
+                <img src="img/DataScientist.png" alt="" className="quiz-img" />
+            </div>
         </div>
     );
 }
