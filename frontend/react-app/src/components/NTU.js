c44a2dfc2a4becbdd740b26c0a368df9d8148d89
brianthad <e0758773@u.nus.edu>
2023-11-09 06:24:23 +0800

    edited overview and university visuals

--- frontend/react-app/src/components/NTU.js
+++ frontend/react-app/src/components/NTU.js
@@ -5,46 +5,49 @@ const NTU = () => {
         <div className="university">
             <h2 className="university-header">NTU</h2>
             <h4 className="university-subheader">Nanyang Technological University</h4>
-            <div className="column-container">
-                <div className="column">Graph 1</div>
-                <div className="column">Text 1</div>
-            </div>
+            <p className="overview-header-text">
+                Data science is a multidisciplinary field that involves the use of various techniques, algorithms, processes, and systems to extract knowledge and insights from structured and unstructured data. 
+            </p>
 
-            <div className="column-container">
-                <div className="column">Text 2</div>
-                <div className="column">Graph 2</div>
-            </div>
+            <div className="overview-inline-block">
 
-            <h4 className="module-distribution">Module Distribution</h4>
-
-            <div className="column-container">
-                <div className="column">Piechart</div>
-                <div className="column">
-                    <h4>25% Finance</h4> <br></br>
-                    <h4>25% Programming</h4> <br></br>
-                    <h4>25% Data Analysis/Modelling</h4> <br></br>
-                    <h4>25% Statistics</h4>
+                <div className="overview-inline-item-1">
+                    <p className="overview-cluster-header-1">General Information</p>
+                    <p className="overview-inline-text-1"> 
+                    Nanyang Technological University (NTU) is one of Singapore's two major national universities. Founded in 1981, it is also the second-oldest autonomous university in the country. NTU is frequently ranked within the world's top 30 universities according to most major international rankings, and is widely-considered to be one of the two most prestigious universities in Singapore, after the National University of Singapore.
+                    </p>
                 </div>
-            </div>
 
-            <h4 className="modules">Modules</h4>
+                <div className="overview-inline-item-2">
+                    <img src="img/ntu-ranking.jpg" className='ntu-img1'/>
+                </div>
 
-            <div className="module-list">
-                <ol>
-                    <li><a href="https://nusmods.com/courses/DSA1101/introduction-to-data-science">NTU MOD 1</a>: Introduction to Data Science</li>
-                    <li><a href="https://nusmods.com/courses/DSA2101/essential-data-analytics-tools-data-visualisation">NTU MOD 2</a>: Essential Data Analytics Tools: Data Visualisation</li>
-                    <li><a href="https://nusmods.com/courses/DSA2102/essential-data-analytics-tools-numerical-computation">NTU MOD 3</a>: Essential Data Analytics Tools: Numerical Computation</li>
-                </ol>
+            </div>   
+
+            <h4 className="university-course-header">Available Data Related Courses</h4>
+
+            <div className='button-course-container'>
+                <a href='https://www.ntu.edu.sg/education/undergraduate-programme/bachelor-of-science-in-data-science-artificial-intelligence'><button className='button-course'>
+                    <p className='course-name'> Data Science and Artificial Intelligence </p>
+                    <p className='course-department'> School of Computer Science and Engineering (SCSE)  </p>
+                    <p className='course-description'> An undergraduate degree programme in Data Science and Artificial Intelligence, based on rigorous training in the synergistic fields of statistics and computer science. The programme, which is run jointly by the School of Computer Science and Engineering and the School of Physical and Mathematical Sciences, targets students who have the vision of using data science and artificial intelligence (AI) to find innovative solutions to society’s pressing challenges. The curriculum provides students with opportunities to solve real-life problems in different applications domains ranging from science and technology, healthcare, business and finance, environmental sustainability, and more. </p>
+                </button></a>
+
+                <a href='https://www.ntu.edu.sg/education/undergraduate-programme/bachelor-of-science-in-economics-and-data-science'><button className='button-course'>
+                    <p className='course-name'>  Economics and Data Sciences </p>
+                    <p className='course-department'> School of Computer Science and Engineering (SCSE) </p>
+                    <p className='course-description'> This is a four-year degree programme in which undergraduate students will read 3 subject areas in Economics, Mathematics and Data Science. The programme provides its students with a strong foundation in Economics and subsequently prepares them not only to handle and deal with big data through Data Science but also develops their ability to make economic sense from their applications in modern large-scale data analysis.  Upon graduation, students are expecting to work as data analysts, data scientists, economists and industry analysts who are skillful in analyzing big data.​ </p>
+                </button></a>
 
             </div>
             
 
-            <h4 className='button-text'>Explore the universities!</h4>
+            <h4 className='button-text'>Explore the universities and its majors below!</h4>
 
             <div className="buttonContainer">
-                <Link to="/NUS"><button className='button'> NUS </button></Link>
+                <Link to="/NUS"><button className='button-uni'> NUS </button></Link>
                 <Link to="/NTU"><button className='button-selected'> NTU </button></Link>
-                <Link to="/SMU"><button className='button'> SMU </button></Link>
+                <Link to="/SMU"><button className='button-uni'> SMU </button></Link>
             </div>
 
         </div>
--- frontend/react-app/src/components/NUS.js
+++ frontend/react-app/src/components/NUS.js
@@ -5,46 +5,65 @@ const NUS = () => {
         <div className="university">
             <h2 className="university-header">NUS</h2>
             <h4 className="university-subheader">National University of Singapore</h4>
-            <div className="column-container">
-                <div className="column">Graph 1</div>
-                <div className="column">Text 1</div>
-            </div>
+            <p className="overview-header-text">
+                Data science is a multidisciplinary field that involves the use of various techniques, algorithms, processes, and systems to extract knowledge and insights from structured and unstructured data. 
+            </p>
 
-            <div className="column-container">
-                <div className="column">Text 2</div>
-                <div className="column">Graph 2</div>
-            </div>
+            <div className="overview-inline-block">
 
-            <h4 className="module-distribution">Module Distribution</h4>
-
-            <div className="column-container">
-                <div className="column">Piechart</div>
-                <div className="column">
-                    <h4>25% Finance</h4> <br></br>
-                    <h4>25% Programming</h4> <br></br>
-                    <h4>25% Data Analysis/Modelling</h4> <br></br>
-                    <h4>25% Statistics</h4>
+                <div className="overview-inline-item-1">
+                    <p className="overview-cluster-header-1">General Information</p>
+                    <p className="overview-inline-text-1"> 
+                    The National University of Singapore (NUS) is a national public collegiate and research university in Singapore. Founded in 1905 as the Straits Settlements and Federated Malay States Government Medical School, NUS is the oldest autonomous university in Singapore. In a historic first for the University, NUS has broken into the worldwide top 10, according to results from the latest Quacquarelli Symonds (QS) World University Rankings (WUR) released on 28 June 2023. Now ranking 8th in the world, NUS is also the top university in Asia.
+                    </p>
                 </div>
+
+                <div className="overview-inline-item-2">
+                    <img src="img/nus-ranking.jpg" className='nus-img1'/>
+                </div>
+
+            </div>   
+
+            <h4 className="university-course-header">Available Data Related Courses</h4>
+
+            <div className='button-course-container'>
+                <a href='https://www.stat.nus.edu.sg/prospective-students/undergraduate-programme/data-science-and-analytics/'><button className='button-course'>
+                    <p className='course-name'> Data Science and Analytics </p>
+                    <p className='course-department'> College of Humanities and Sciences (CHS) </p>
+                    <p className='course-description'> Bachelor of Science (Honours) with Major in Data Science and Analytics. The four-year direct Honours programme is designed to prepare graduates who are ready to acquire, manage and explore data that will inspire change around the world. Students will read courses in Mathematics, Statistics and Computer Science, and be exposed to the interplay between these three key areas in the practice of data science. </p>
+                </button></a>
+
+                <a href='https://www.stat.nus.edu.sg/prospective-students/undergraduate-programme/data-science-and-analytics/'><button className='button-course'>
+                    <p className='course-name'> Data Science and Economics </p>
+                    <p className='course-department'> College of Humanities and Sciences (CHS) </p>
+                    <p className='course-description'> Bachelor of Science (Honours) with Major in Data Science and Economics. The Data Science and Economics (DSE) cross-disciplinary programme (XDP) aims to produce students who have strong foundation knowledge in data science and economics as well as hands-on experience with empirical analysis of economic data, to analyse and interpret the local and global impact of data on individuals, organisation, society and the global economic ecosystem. </p>
+                </button></a>
+
+                <a href='https://www.math.nus.edu.sg/ug/majmin/primajors/qf/'><button className='button-course'>
+                    <p className='course-name'> Quantitative Finance </p>
+                    <p className='course-department'> College of Humanities and Sciences (CHS) </p>
+                    <p className='course-description'> Bachelor of Science (Honours) with Major in Quantitative Finance. The NUS Quantitative Finance curriculum gives you an integrated overview of how mathematical methods and computing techniques are applied to finance, including mathematical theory, statistical tools, computing theory, financial principles and core financial product knowledge.</p>
+                </button></a>
+
+                <a href='https://www.stat.nus.edu.sg/'><button className='button-course'>
+                    <p className='course-name'> Statistics </p>
+                    <p className='course-department'> College of Humanities and Sciences (CHS) </p>
+                    <p className='course-description'> Bachelor of Science (Honours) with Major in Statistics. Professors in the department prepare students in this four-year direct Honours programme for the workplace by teaching them how to collect, analyse and present data. Students learn how to extract information from surveys, databases and carefully designed experiments, in order to obtain understanding of the underlying phenomenon, or to decide on a suitable course of action. They learn programming, problem solving and data visualization skills, and they become sensitive to the applications at hand. </p>
+                </button></a>
+
+                <a href='https://www.comp.nus.edu.sg/programmes/ug/ba/curr/'><button className='button-course'>
+                    <p className='course-name'> Business Analytics </p>
+                    <p className='course-department'> School of Computing </p>
+                    <p className='course-description'> The Bachelor of Science (Business Analytics) degree programme is an inter-disciplinary undergraduate degree programme offered by the School of Computing with participation from the Business School, Faculty of Engineering, Faculty of Science, and Faculty of Arts and Social Sciences. This is a four-year direct honours programme which offers a common two-year broad-based inter-disciplinary curriculum where all students will read courses in Mathematics, Statistics, Economics, Accounting, Marketing, Decision Science, Industrial and Systems Engineering, Computer Science and Information Systems. </p>
+                </button></a>
             </div>
 
-            <h4 className="modules">Modules</h4>
-
-            <div className="module-list">
-                <ol>
-                    <li><a href="https://nusmods.com/courses/DSA1101/introduction-to-data-science">DSA1101</a>: Introduction to Data Science</li>
-                    <li><a href="https://nusmods.com/courses/DSA2101/essential-data-analytics-tools-data-visualisation">DSA2101</a>: Essential Data Analytics Tools: Data Visualisation</li>
-                    <li><a href="https://nusmods.com/courses/DSA2102/essential-data-analytics-tools-numerical-computation">DSA2102</a>: Essential Data Analytics Tools: Numerical Computation</li>
-                </ol>
-
-            </div>
-            
-
-            <h4 className='button-text'>Explore the universities!</h4>
+            <h4 className='button-text'>Explore the universities and its majors below!</h4>
 
             <div className="buttonContainer">
                 <Link to="/NUS"><button className='button-selected'> NUS </button></Link>
-                <Link to="/NTU"><button className='button'> NTU </button></Link>
-                <Link to="/SMU"><button className='button'> SMU </button></Link>
+                <Link to="/NTU"><button className='button-uni'> NTU </button></Link>
+                <Link to="/SMU"><button className='button-uni'> SMU </button></Link>
             </div>
 
         </div>
